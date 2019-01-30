43423008d13125559beffa6848fc74ad3f0eed8a
rewantkedia <kediarewant@gmail.com>
2019-01-31 00:08:18 +0530

    Header props prblem fixed

--- src/components/header.js
+++ src/components/header.js
@@ -18,18 +18,26 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Redirect } from "react-router";
 export default class Header1 extends Component {
   state = {
-    
     loggedin: false
   };
-  constructor(props){
+  constructor(props) {
     super(props);
-    
-    this.state.username=this.props.username;
+
+    this.state.username = this.props.username;
     this.state.loggedin = this.props.loggedin;
     console.log("Header");
     console.log(props);
   }
-  
+
+  componentWillReceiveProps(props) {
+    console.log("PROPS", props);
+    // this.setState({
+    //   loggedin: props.loggedin,
+    //   username: props.username
+    // });
+    this.state.loggedin = props.loggedin;
+    this.state.username = props.username;
+  }
   // componentDidMount(){
   //   const user_axios = axios.create({
   //     withCredentials: true,
@@ -40,7 +48,7 @@ export default class Header1 extends Component {
   //   });
   //   user_axios.get("localhost://8080/user/").then(function(response){
   //     console.log("RESPONSE",response.success)
-  //   if(response.success==0)  
+  //   if(response.success==0)
   //   {this.setState({
   //       loggedin: false,
   //     });}
@@ -54,37 +62,39 @@ export default class Header1 extends Component {
   //   })
   // }
   render() {
-    
     return (
-      
-      <div className="container-fluid"  >
+      <div className="container-fluid">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <Link to={"/"} className="navbar-brand">
             Online Judge
           </Link>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav  headerNav" >
+            <ul className="navbar-nav  headerNav">
               <li className="nav-item">
-              { !this.state.loggedin ?  <Link to={"/"} className="nav-link" >
-                  Home
-                </Link>: null }
-               
+                {!this.state.loggedin ? (
+                  <Link to={"/"} className="nav-link">
+                    Home
+                  </Link>
+                ) : null}
               </li>
               <li className="nav-item">
-              { !this.state.loggedin ?  <Link to={"/login"} className="nav-link" >
-                  Login
-                </Link>: null }
-                
+                {!this.state.loggedin ? (
+                  <Link to={"/login"} className="nav-link">
+                    Login
+                  </Link>
+                ) : null}
               </li>
               <li className="nav-item right">
-              { !this.state.loggedin ?  <Link to={"/signup"} className="nav-link" >
-                  SignUp
-                </Link>: null }
-                
+                {!this.state.loggedin ? (
+                  <Link to={"/signup"} className="nav-link">
+                    SignUp
+                  </Link>
+                ) : null}
               </li>
               <li className="nav-item right">
-              { this.state.loggedin ?  <Drop name={this.state.username} />: null }
-               
+                {this.state.loggedin ? (
+                  <Drop name={this.state.username} />
+                ) : null}
               </li>
             </ul>
           </div>
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,86 +1,88 @@
-import React, { Component } from 'react';
-import Header1 from '../components/header';
+import React, { Component } from "react";
+import Header1 from "../components/header";
 import axios from "axios";
-import { Divider } from 'semantic-ui-react';
+import { Divider } from "semantic-ui-react";
 
 export default class Homepage extends Component {
-    state = {
-        username: "",
-        loggedin : false,
-        complete:false
-    }
-    getuser = () => {
-        console.log("HEREasdjnakjsdnk");
-        const user_axios = axios.create({
-          withCredentials: true,
-            headers: {
-              Accept: "application/json",
-              "Content-Type": "x-www-form-urlencoded"
-            },
+  state = {
+    username: "",
+    loggedin: false,
+    complete: false
+  };
+  getuser = () => {
+    console.log("HEREasdjnakjsdnk");
+    const user_axios = axios.create({
+      withCredentials: true,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "x-www-form-urlencoded"
+      }
+    });
+    console.log("asdasd00");
+    user_axios.get("http://localhost:8080/user/").then(function(response) {
+      response = response.data;
+      console.log("RESPONSE", response);
+      if (response.success === 0) {
+        this.setState({
+          loggedin: false
         });
-        console.log("asdasd00");
-        user_axios.get("http://localhost:8080/user/").then(function(response){
-            response = response.data;
-          console.log("RESPONSE",response)
-        if(response.success===0)  
-        {this.setState({
-            loggedin: false,
-          });}
-          else{
-            console.log("HERERERER")
-            this.setState({
-              loggedin:true,
-              username:response.user,
-             
-            })
-            console.log("Changed Visibility");
-          }
-          this.setState({complete:true})
-          console.log("asdmasjbdajasd");
-          console.log(this.state);
-        })
-    }
-    constructor(props)
-    {
-        super(props);
-    }
-    componentDidMount(){
-        // this.getuser();
-        console.log("HEREasdjnakjsdnk");
-        const user_axios = axios.create({
-          withCredentials: true,
-            headers: {
-              Accept: "application/json",
-              "Content-Type": "x-www-form-urlencoded"
-            },
+      } else {
+        console.log("HERERERER");
+        this.setState({
+          loggedin: true,
+          username: response.user
         });
-        console.log("asdasd00");
-        user_axios.get("http://localhost:8080/user/").then(function(response){
-            response = response.data;
-          console.log("RESPONSE",response)
-        if(response.success===0)  
-        {
-            console.log("FAILURE1")
-            this.setState({
-            loggedin: false,
-          });
-          console.log("FAILURE2")
-        }
-          else{
-            console.log("HERERERER");
-            console.log(response.user);
-            this.setState({
-              loggedin:true,
-              username:response.user
-            });
-            console.log("Changed Visibility");
-          }
-          this.setState({complete:true})
-          console.log("asdmasjbdajasd");
-          console.log(this.state);
-        })
-    }
-    render() {
-        return <Header1 username={this.state.username} loggedin={this.state.loggedin} />
-    }
+        console.log("Changed Visibility");
+      }
+      this.setState({ complete: true });
+      console.log("asdmasjbdajasd");
+      console.log(this.state);
+    });
+  };
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    // this.getuser();
+    console.log("HEREasdjnakjsdnk");
+    const user_axios = axios.create({
+      withCredentials: true,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "x-www-form-urlencoded"
+      }
+    });
+    var self = this;
+    console.log("asdasd00");
+    user_axios.get("http://localhost:8080/user/").then(function(response) {
+      response = response.data;
+      console.log("RESPONSE", response);
+      if (response.success === 0) {
+        console.log("FAILURE1");
+        self.setState({
+          loggedin: false
+        });
+        console.log("FAILURE2");
+      } else {
+        console.log("HERERERER");
+        console.log(response.user);
+        self.setState({
+          loggedin: true,
+          username: response.user
+        });
+        console.log("Changed Visibility");
+      }
+      this.setState({ complete: true });
+      console.log("asdmasjbdajasd");
+      console.log(this.state);
+    });
+  }
+  render() {
+    const { username, loggedin } = this.state;
+    return (
+      <div>
+        <Header1 username={username} loggedin={loggedin} />
+      </div>
+    );
+  }
 }
